3cc3ed3dd61b467baabb0d1abd01f446c152b554
Carlos Petit <ca.alberto.p@gmail.com>
2022-06-20 19:37:55 -0400

    updategit ignore

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -5,7 +5,27 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: 0,
+        background: "red",
+        right: 0,
+        width: "30%",
+        height: "20%",
+        marginBottom: "0.5em",
+        marginRight: "0.5em",
+        borderRadius: "10px",
+      }}
+    >
+      <h6>Como evaluas la experiencia en xxxxx </h6>
+      <div>
+        <input type="radio" name="radio" value="1" />
+        <label>1</label>
+      </div>
+    </div>
+  );
 };
 
 export default Button;
