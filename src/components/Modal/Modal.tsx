6764b687d9adcc80b535db73719e26f91bbbe058
Carlos Petit <ca.alberto.p@gmail.com>
2022-06-20 20:27:30 -0400

    update component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Modal.css'
 export interface ButtonProps {
   label: string;
 }
@@ -10,19 +10,139 @@ const Button = (props: ButtonProps) => {
       style={{
         position: "absolute",
         bottom: 0,
-        background: "red",
         right: 0,
-        width: "30%",
-        height: "20%",
         marginBottom: "0.5em",
         marginRight: "0.5em",
         borderRadius: "10px",
+        padding: "0.5em 1em 0.5em 1em",
+        borderColor: "#fff",
+        borderStyle: "solid",
+        borderWidth: "1px",
+        width: "auto",
+        minWidth: "20em",
       }}
     >
-      <h6>Como evaluas la experiencia en xxxxx </h6>
-      <div>
-        <input type="radio" name="radio" value="1" />
-        <label>1</label>
+      <div style={{ padding: "0.5em 0.5em 0.5em 0em" }}>
+        <label>Place here your poll</label>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "1em",
+          marginBottom: "0.5em",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="1" />
+          <label>1</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="2" />
+          <label>2</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="3" />
+          <label>3</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="4" />
+          <label>4</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="5" />
+          <label>5</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="6" />
+          <label>6</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="7" />
+          <label>7</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="8" />
+          <label>8</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="9" />
+          <label>9</label>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            marginRight: "0.5em",
+            marginLeft: "0.5em",
+          }}
+        >
+          <input type={"radio"} name="radio" value="10" />
+          <label>10</label>
+        </div>
       </div>
     </div>
   );
